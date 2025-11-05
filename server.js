82bfdeaf270dd225808b75932e9815a6560e6494
catsthegoat <emmahefter1@gmail.com>
2025-11-05 17:10:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,24 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
-app.use('/', createProxyMiddleware({
-  target: 'https://example.com',
-  changeOrigin: true,
-  router: (req) => req.query.url || 'https://motox3m.gitlab.io',
-  pathRewrite: { '^/': '' }
-}));
+// Enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
-app.listen(10000);
+// Proxy route
+app.get('/', (req, res) => {
+  const target = req.query.url || 'https://motox3m.gitlab.io';
+  createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    pathRewrite: { '^/': '' },
+    onProxyRes: (proxyRes) => {
+      proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+    }
+  })(req, res);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Proxy running on port ${PORT}`));
